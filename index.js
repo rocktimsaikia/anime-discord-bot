c139b0c7e85b65ea037e744d8c5fc34d78cb02b2
RocktimSaikia <rocktimthedev@gmail.com>
2021-03-01 11:04:59 -1100

    :wrench: (hooks) configure xo and commitizen locally

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const fetch = require('isomorphic-unfetch');
-
 const client = new Discord.Client();
-client.login(process.env.BOT_TOKEN)
-    .then('Bot is alive')
-    .catch(err => console.err(err));
 
-client.on('message', async (message) => {
-    if(message.author.bot) return;
-    if(!message.content.startsWith('!')) return;
+client.on('ready', () => {
+	console.info(`Logged in as ${client.user.tag} 🚀`);
+});
 
-    const body = message.content.slice(1);
-    const args = body.split(' ');
-    const command = args.shift().toLowerCase();
+client.on('message', async message => {
+	if (message.author.bot || !message.content.startsWith('!')) {
+		return;
+	}
 
-    if(command === 'random'){
-        try {
-            const data  = await fetch('https://animechan.vercel.app/api/random');
-            const res = await data.json();
-            message.reply(`\n\n> ***${res.quote}***\n\n - ✍️ ${res.character} (${res.anime})`);   
-        } catch (error) {
-            console.error(error);
-        }
-    }
-})
+	const body = message.content.slice(1);
+	const args = body.split(' ');
+	const command = args.shift().toLowerCase();
+
+	if (command === 'random') {
+		try {
+			const data = await fetch('https://animechan.vercel.app/api/random');
+			const respose = await data.json();
+			message.reply(`\n\n> ***${respose.quote}***\n\n - ✍️ ${respose.character} (${respose.anime})`);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+});
+
+client.login(process.env.BOT_TOKEN);
